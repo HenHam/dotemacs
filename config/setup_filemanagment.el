2a9b7e2a58877295b7e6a58e5c8b20b7025885e1
Hendrik Hamaekers <hendrikhamaekers@web.de>
2019-09-14 18:00:01 +0200

    filemanagment neotree
    - show hidden files folders

--- config/setup_filemanagment.el
+++ config/setup_filemanagment.el
@@ -1,6 +1,25 @@
+;;
+;; File Buffer Encoding
+;;
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+
+;;
+;; File-Explorer
+;;
 (use-package
   neotree
   
-  :ensure t)
+  :ensure t
+
+  :config
+  (setq neo-show-hidden-files t)
+  )
 
 (provide 'setup_filemanagment)
