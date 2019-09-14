57994473f19c5ea889098276cc160d3ec8def52c
Hendrik Hamaekers <hendrikhamaekers@web.de>
2019-09-14 19:02:20 +0200

    editor-functions
    - scrolling steps

--- config/setup_editor-functions.el
+++ config/setup_editor-functions.el
@@ -1,4 +1,12 @@
 ;;
+;; Scrolling
+;;
+(let ((n 5))
+  (setq scroll-step 1)
+  (setq scroll-conservatively n)
+  (setq scroll-margin n))
+
+;;
 ;; Tabs and Lines
 ;;
 (setq-default indent-tabs-mode nil)
