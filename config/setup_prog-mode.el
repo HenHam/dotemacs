6794f584581fda0e9c4c0d1a1ce8ffe55471adcd
Hendrik Hamaekers <hendrikhamaekers@web.de>
2019-09-14 19:33:28 +0200

    prog-mode functions
    - paredit

--- config/setup_prog-mode.el
+++ config/setup_prog-mode.el
@@ -27,4 +27,31 @@
 ;; Start ist.
 ;; (cider-repl-mode . rainbow-delimiters-mode)
 
+
+;;
+;; Paredit ...
+;;
+(use-package
+  paredit
+
+  :ensure
+  t
+
+  :defer
+  t
+
+  :hook
+  ((prog-mode . paredit-mode)
+   )
+  ;; XXX hook erweitern wenn clojure unterstütz
+  ;; wird
+  ;;(prog-mode . paredit-mode)
+  ;; (cider-mode . paredit-mode)
+  ;; (clojure-mode . paredit-mode)
+  ;; (cider-repl-mode . paredit-mode)
+  ;; (with-editor-mode . paredit-mode)
+
+  )
+
+
 (provide 'setup_prog-mode)
